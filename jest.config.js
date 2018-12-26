7b0df9ffad76ed9a567010d172eaa4335756708e
rogerbf <roger@bergfa.lk>
2018-12-26 20:47:38 +0100

    separate development tests

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,6 @@ const { NODE_ENV } = process.env
 module.exports = {
   testRegex:
     NODE_ENV === `development`
-      ? `(/tests/.*\\.spec.js)$`
-      : `tests/index.spec.js`,
+      ? `(/tests/development/.*\\.spec.js)$`
+      : `/tests/index.spec.js`,
 }
