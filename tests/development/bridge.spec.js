c8ad833722c36db8213fbfa1a864a2e11d83f2eb
rogerbf <roger@bergfa.lk>
2019-01-05 12:23:28 +0100

    initial

---
+++ tests/development/bridge.spec.js
@@ -0,0 +1,20 @@
+import bridge from "../../source/bridge"
+
+describe(`bridge`, () => {
+  it(`is a function`, () => {
+    expect(typeof bridge).toEqual(`function`)
+  })
+
+  it(`returns a middleware`, () => {
+    const dispatch = jest.fn((...args) => args)
+
+    const middleware = bridge(dispatch)
+
+    const next = jest.fn((...args) => args)
+
+    const initialized = middleware(() => {})(next)
+
+    expect(initialized(1, 2, 3)).toEqual([ 1, 2, 3 ])
+    expect(dispatch).toHaveBeenCalledWith(1, 2, 3)
+  })
+})
