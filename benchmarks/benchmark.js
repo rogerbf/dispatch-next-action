1063495a876c5035f6708036fdb6bcd5eaaac04f
rogerbf <roger@bergfa.lk>
2019-02-18 22:07:37 +0100

    use bench

--- benchmarks/benchmark.js
+++ benchmarks/benchmark.js
@@ -1,43 +1,50 @@
-const { invoke, Suite } = require(`benchmark`)
-const {
-  staticMiddleware,
-  dynamicMiddleware,
-} = require(`../build/dispatch-next-action`)
+const { dynamicMiddleware, staticMiddleware } = require(`../`)
+const middleware = require(`./middleware`)
 
-const createMiddleware = (count = 1) =>
-  Array(count).fill(() => next => action => next(action))
+const staticOneMiddleware = staticMiddleware(...middleware[0])
+const dynamicOneMiddleware = dynamicMiddleware(...middleware[0])
+const staticTwoMiddleware = staticMiddleware(...middleware[1])
+const dynamicTwoMiddleware = dynamicMiddleware(...middleware[1])
+const staticFourMiddleware = staticMiddleware(...middleware[2])
+const dynamicFourMiddleware = dynamicMiddleware(...middleware[2])
+const staticEightMiddleware = staticMiddleware(...middleware[3])
+const dynamicEightMiddleware = dynamicMiddleware(...middleware[3])
+const staticSixteenMiddleware = staticMiddleware(...middleware[4])
+const dynamicSixteenMiddleware = dynamicMiddleware(...middleware[4])
 
-const middlewareCounts = [ 1, 2, 4, 8, 16 ]
+exports.compare = {
+  "staticMiddleware (1)": () => {
+    staticOneMiddleware()
+  },
+  "dynamicMiddleware (1)": () => {
+    dynamicOneMiddleware()
+  },
+  "staticMiddleware (2)": () => {
+    staticTwoMiddleware()
+  },
+  "dynamicMiddleware (2)": () => {
+    dynamicTwoMiddleware()
+  },
+  "staticMiddleware (4)": () => {
+    staticFourMiddleware()
+  },
+  "dynamicMiddleware (4)": () => {
+    dynamicFourMiddleware()
+  },
+  "staticMiddleware (8)": () => {
+    staticEightMiddleware()
+  },
+  "dynamicMiddleware (8)": () => {
+    dynamicEightMiddleware()
+  },
+  "staticMiddleware (16)": () => {
+    staticSixteenMiddleware()
+  },
+  "dynamicMiddleware (16)": () => {
+    dynamicSixteenMiddleware()
+  },
+}
 
-invoke(
-  middlewareCounts.map(middlewareCount => {
-    const suite = new Suite(`${ middlewareCount } middleware`)
-    const staticDispatch = staticMiddleware(
-      ...createMiddleware(middlewareCount)
-    )
-    const dynamicDispatch = dynamicMiddleware(
-      ...createMiddleware(middlewareCount)
-    )
-    const action = {}
+exports.compareCount = true
 
-    suite
-      .add(`static`, () => {
-        staticDispatch(action)
-      })
-      .add(`dynamic`, () => {
-        dynamicDispatch(action)
-      })
-
-    return suite
-  }),
-  {
-    name: `run`,
-    queued: true,
-    onCycle: event => {
-      const suiteName = event.target.name
-      for (let i = 0; i < event.target.length; i++) {
-        console.log(suiteName, String(event.target[i.toString()]))
-      }
-    },
-  }
-)
+require(`bench`).runMain()
